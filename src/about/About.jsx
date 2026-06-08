d7b800f4adc0a4751fd64aa4cea7311690c3baf5
Nike <nikehallberg9@gmail.com>
2026-06-08 23:42:46 +0200

    css

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './About.css'
+import icon from '../assets/icon.png'
 
 const About = () => {
   return (
     <div className='about-page'>
       <h2>About Me</h2>
+      <img src={icon} alt="About icon" />
       <p>This is the about page inside the portfolio app.</p>
     </div>
   )
